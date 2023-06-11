2afebc2c8476f153e2ea3f5c4701fe2241237359
Frank Willard <fcw@gaia.egr.duke.edu>
2023-06-10 22:54:29 -0400

    Added scripts

--- Synthetic-Image-Generation/run_synthetic_data_generation.py
+++ Synthetic-Image-Generation/run_synthetic_data_generation.py
@@ -17,7 +17,6 @@ gp_gan_dir = "/home/fcw/Blended-Synthetic-Imagery-for-Climate-Object-Detection/S
 # Dataset arguments
 domains = "EM NW SW"
 
-experiment_name = "first_experiment"
 # generate_unique_src_augmentations = True
 # verbose = True
 
@@ -26,8 +25,7 @@ experiment_name = "first_experiment"
 num_objects_to_sample_per_image_constant = 3
 num_synthetic_images_per_domain = 5
 objects_augmenter_has_access_to = 100
-
-#cmd = f"python run_gp_gan.py --src_image {src_img} --dst_image \"{dst_img}\" --mask_image {mask_img} --blended_image {blended_img_out_path}"
+experiment_name = "first_experiment"
 
 cmd = f"python3 synthetic_dataset_generation.py --implantable-objects-dir {implantable_objects_dir} --augmented-images-results-dir {augmented_images_results_dir} --random-seed {random_seed} --num-objects-to-sample-per-image-constant {num_objects_to_sample_per_image_constant} --background-images-dir {background_images_dir} --final-results-dir {final_results_dir} --gp_gan_dir {gp_gan_dir} --domains {domains} --num-synthetic-images-per-domain {num_synthetic_images_per_domain} --experiment_name {experiment_name} --generate-unique-src-augmentations --generate_all_augmentations_first --verbose"
 
